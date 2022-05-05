2df0db35db16185d560576628133a1f58f8f21d3
SD-Jonatha <thiago.infortec@gmail.com>
2022-05-05 15:53:46 -0300

    implementacao email

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,46 @@
+import { prisma } from './prisma';
+import nodemailer from 'nodemailer'
 import express from 'express'
 
 const app = express();
+app.use(express.json())
 
-app.get('/users', (req, res)=> {
-  return res.send('Hello World')
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "06bcf49ccaebe9",
+    pass: "d17c974bd33d1c"
+  }
+});
+
+app.post('/feedbacks', async (req, res)=> {
+
+  const {type, comment, screenshot} = req.body;
+ const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Thiago Santos <thiago.infortec@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 106px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('/n')
+  })
+
+  return res.status(201).json({data : feedback})
 })
+
+
 
 app.listen(3333, ()=> {
   console.log('HTTP server is running')
